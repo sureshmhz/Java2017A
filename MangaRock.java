0b0030707df4bc69008ed1bcaff88b6f231c34ac
PrabinRai <chokhang777@gmail.com>
2015-04-02 19:12:40 +0545

    Assignment

---
+++ MangaRock.java
@@ -0,0 +1,27 @@
+package com;
+import java.util.Scanner;
+
+/**
+ * Created by DELL on 4/2/2015.
+ */
+public class MangaRock {
+    public static void main (String[] args){
+
+      String userData;
+      String readManga;
+      String manga;
+
+      new
+
+       String readManga( userData ){
+
+        }
+
+
+        String manga()
+        {
+
+        }
+    }
+
+}
