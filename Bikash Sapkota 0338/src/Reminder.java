ba35b80003de553d33dbeed364d3794955d67e1c
Software Club <softwareclub@deerwalk.edu.np>
2015-04-02 18:39:51 +0545

    COmpleted.

---
+++ Bikash Sapkota 0338/src/Reminder.java
@@ -0,0 +1,26 @@
+/**
+ * Created by Bikash on 2/04/2015.
+ */
+public class Reminder {
+
+    String time;
+    public void setTime(){
+
+    }
+
+    public void ring(){
+
+    }
+
+    public void stopReminder(){
+
+    }
+
+    public void snooze(){
+
+    }
+    public void setNotes(){
+
+    }
+
+}
---
+++ Bikash Sapkota 0338/src/Routine.java
@@ -0,0 +1,32 @@
+/**
+ * Created by Bikash on 2/04/2015.
+ */
+public class Routine {
+    String date, time, week;
+
+    public void setRotine(){
+
+    }
+
+    public void deleteRoutine(){
+
+    }
+
+    public void showToday(){
+
+    }
+
+
+    public void thisTime(){
+
+    }
+
+    public void showRoutine(String date){
+
+    }
+
+    public void nextDayRoutine(){
+
+    }
+
+}
