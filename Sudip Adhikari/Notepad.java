eb099e0ce8637634d9e6bdfebb3e3bb4296b3dd7
sudipadhikari <sudip.adhikari@deerwalk.edu.np>
2015-04-03 13:56:16 +0545

    assignment was added

---
+++ Sudip Adhikari/Notepad.java
@@ -0,0 +1,64 @@
+/**
+ * Created by Sunil on 4/3/2015.
+ */
+public class Notepad {
+    public static void main(String[] args) {
+        Edit smallEdit=new Edit();
+        smallEdit.editContent();
+        //To Edit the content in the file//
+        smallEdit.add();
+        //To add the content in the file
+        smallEdit.delete();
+        //To delete the file
+        smallEdit.save();
+        //To save the file
+        smallEdit.copy();
+        //To copy the file
+        smallEdit.paste();
+        //To paste the file
+        smallEdit.cut();
+        //To paste the file//
+        smallEdit.chooseFont();
+        //To choose the required font
+
+
+    }
+}
+
+class Edit{
+    editContent(){
+        //code
+    }
+    
+    add(){
+        //code
+    }
+    delete(){
+        //code
+    }
+    
+    save(){
+        //save
+    }
+    
+    copy(){
+        //code
+    }
+    
+    cut(){
+        //code
+    }
+    
+    paste(){
+        //code
+    }
+    chooseFont(){
+        //code
+    }
+    
+    
+    
+    
+    
+}
+
